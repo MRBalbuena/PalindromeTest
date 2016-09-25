116f9fb5241e5e94fa3994199c5605775218f6fb
mrbalbuena <mrbalbuena@hotmail.com>
2016-09-25 19:57:20 +0100

    Added webAPI, removed unnecessary scripts

---
+++ Test.WebAPI/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TestDto;
+
+namespace Test.WebAPI.Controllers
+{
+    public class TestController : ApiController
+    {
+        public List<PalindromesDto> GetPalindromes(string value)
+        {
+            var palindromes = new Palindrome();
+            return palindromes.GetTopPalidromes(value, 3);
+        }
+    }
+}
